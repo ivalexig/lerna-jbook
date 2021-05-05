b9f14282a92602fc83e83083d9162a21cfe4dabb
ivalexig <ivalexig@gmail.com>
2021-05-05 22:39:02 +1200

    added monaco editor

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,36 @@
+import MonacoEditor from '@monaco-editor/react';
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+
+  return (
+    <MonacoEditor
+      value={initialValue}
+      editorDidMount={onEditorDidMount}
+      theme="dark"
+      language="javascript"
+      height="500px"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 14,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import CodeEditor from './components/code-editor';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
@@ -13,15 +14,15 @@ const App = () => {
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
+    });
   };
 
   useEffect(() => {
     startService();
-  }, [])
+  }, []);
 
   const onClick = async () => {
-    if(!ref.current){
+    if (!ref.current) {
       return;
     }
 
@@ -35,8 +36,8 @@ const App = () => {
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
-      }
-    })
+      },
+    });
 
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
@@ -63,16 +64,24 @@ const App = () => {
     </html>
   `;
 
-  return <div>
-    <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+  return (
     <div>
-      <button onClick={onClick}>Submit</button>  
-    </div>  
-    <iframe ref={iframe} title="code-preview" sandbox="allow-scripts" srcDoc={html}/>
-  </div>;
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      {/* <textarea value={input} onChange={e => setInput(e.target.value)}></textarea> */}
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe
+        ref={iframe}
+        title="code-preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(
-  <App/>,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
