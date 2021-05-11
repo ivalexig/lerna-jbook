a67efedccdeb8ce31525ab8bbe0b6c864496cfaf
ivalexig <ivalexig@gmail.com>
2021-05-11 22:33:33 +1200

    reducer logic

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,17 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { ActionType } from './action-types';
 import reducers from './reducers';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// Reducer testing
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: 'code',
+//   },
+// });
+
+// console.log(store.getState());
